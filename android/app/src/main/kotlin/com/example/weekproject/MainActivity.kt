d06ac2e22854e5142668b65bfde7abce13030130
Amoo .O. Faruk <bookwarmdev@gmail.com>
2021-11-17 04:59:33 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/weekproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weekproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+const kPrimaryColor = Color(0xFFFE990A);
+const kBorderColor = Color(0xFFBDBDBD);
